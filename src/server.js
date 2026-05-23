80ae52fade381b880f39720aba2c0977cd75181d
Benedict Schönenberger <benedict.schoenenberger@ost.ch>
2026-05-23 09:42:25 +0900

    Add dependencies cors and dotenv  to package.json, switched port to 8080

--- src/server.js
+++ src/server.js
@@ -20,7 +20,7 @@ app.get("/api/health", (req, res) => { //Endpoint route used to check the backen
   });
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
