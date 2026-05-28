fde19ea2d5cab684fa5bbddb6970ef7203d77ebb
JUNU <m.l02n23@gmail.com>
2026-05-28 14:19:32 +0900

    feat (shop): implement user purchase

--- src/services/shopService.js
+++ src/services/shopService.js
@@ -1,8 +1,9 @@
 const shopItemModel = require("../models/shopItemModel");
 const userItemModel = require("../models/userItemModel");
+const userModel = require("../models/userModel");
 
 // Handles the business logic for purchasing an item
-async function purchaseItem({ user_id, item_id}) {
+async function purchaseItem({user_id, item_id}) {
 
   // 1) Check whether the item_id is valid
   const item = await shopItemModel.findById(item_id);
@@ -20,23 +21,31 @@ async function purchaseItem({ user_id, item_id}) {
   }
 
   // 3) TODO: Check whether the user has enough will_balance
-  // Example later:
-  // const user = await userModel.findById(user_id);
-  // if (user.will_balance < item.price) {
-  //   const error = new Error("Insufficient will balance");
-  //   error.statusCode = 409;
-  //   throw error;
-  // }
+  const user = await userModel.findById(user_id);
+  if (user.will_balance < item.price) {
+    const error = new Error("Insufficient will balance");
+    error.statusCode = 409;
+    throw error;
+  }
 
   // 4) Add item to user's inventory
   // If the user already owns the item, increase quantity.
-  let purchased_at = new Date().toISOString();
+  let purchased_at =  new Date().toISOString().split('T')[0];
   await userItemModel.create({
     user_id,
     item_id,
     quantity: 1,
     purchased_at
   });
+
+  // decrease will
+  let decrease_result = await userModel.update(user_id, "will_balance", user.will_balance - item.price);
+
+  if(decrease_result === false){
+    const error = new Error("Database Error");
+    error.statusCode = 500;
+    throw error; 
+  }
 
   return {
     user_id,
