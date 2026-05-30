33c80297075094fc17860414d43e967e7f131b30
JUNU <m.l02n23@gmail.com>
2026-05-30 15:14:45 +0900

    use db directly instead of getDb in model

--- src/models/userItemModel.js
+++ src/models/userItemModel.js
@@ -57,18 +57,18 @@ function getDb() {
 
 //return all items based on basic condition
 async function getAll(user_id){
-    return getDb().all("SELECT * FROM user_items WHERE user_id = ?", [user_id]);
+    return await userItemsDb.all("SELECT * FROM user_items WHERE user_id = ?", [user_id]);
     
 }
 // Returns item information by id, or undefined if no row matches. (READ)
 async function findByIds(user_id, item_id) {
-    return await getDb().get("SELECT * FROM user_items WHERE item_id = ? AND user_id = ?", [item_id, user_id]);
+    return await userItemsDb.get("SELECT * FROM user_items WHERE item_id = ? AND user_id = ?", [item_id, user_id]);
 }
 
 // Inserts a purchased item into user_items.
 // If the user already owns the item, increase its quantity.
 async function create({ user_id, item_id, quantity = 1, purchased_at}) {
-    const result = await getDb().run(
+    const result = await userItemsDb.run(
         `
         INSERT INTO user_items (
             user_id,
@@ -90,13 +90,13 @@ async function create({ user_id, item_id, quantity = 1, purchased_at}) {
 // Deletes an item by item_id. Returns true if a row was actually removed, (DELETE)
 // false if no row matched the id.
 async function remove(user_id, item_id) {
-    const result = await getDb().run("DELETE FROM user_items WHERE user_id = ? AND item_id = ?", [user_id, item_id]);
+    const result = await userItemsDb.run("DELETE FROM user_items WHERE user_id = ? AND item_id = ?", [user_id, item_id]);
     return result.changes > 0;
 }
 
 // Updates item information (UPDATE)
 async function update({user_id, item_id, quantity, is_equipped, purchased_at}) {
-    const result = await getDb().run(
+    const result = await userItemsDb.run(
         `
     UPDATE user_items
     SET
